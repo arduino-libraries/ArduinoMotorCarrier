42d3ddc5c12422931d4d78d78af375dc83a4dec6
Martino Facchin <m.facchin@arduino.cc>
2018-12-04 10:57:13 +0100

    Add example test sketches

---
+++ examples/Motor_test/Motor_test.ino
@@ -0,0 +1,92 @@
+#include <MKRMotorCarrier.h>
+#define INTERRUPT_PIN 6
+
+//Variable to store the battery voltage
+static int batteryVoltage;
+
+//Variable to change the motor speed and direction
+static int duty = 0;
+
+void setup() 
+{
+  //Serial port initialization
+  Serial.begin(115200);
+  while (!Serial);
+
+  //Establishing the communication with the motor shield
+  if (controller.begin()) 
+    {
+      Serial.print("MKR Motor Shield connected, firmware version ");
+      Serial.println(controller.getFWVersion());
+    } 
+  else 
+    {
+      Serial.println("Couldn't connect! Is the red led blinking? You may need to update the firmware with FWUpdater sketch");
+      while (1);
+    }
+
+  // Reboot the motor controller; brings every value back to default
+  Serial.println("reboot");
+  controller.reboot();
+  delay(500);
+  
+  //Take the battery status
+  float batteryVoltage = (float)battery.getConverted();
+  Serial.print("Battery voltage: ");
+  Serial.print(batteryVoltage);
+  Serial.print("V, Raw ");
+  Serial.println(battery.getRaw());
+}
+
+
+void loop() {
+  
+  //Take the battery status
+  float batteryVoltage = (float)battery.getConverted();
+  
+  //Reset to the default values if the battery level is lower than 11V
+  if (batteryVoltage < 11) 
+  {
+    Serial.println(" ");
+    Serial.println("WARNING: LOW BATTERY");
+    Serial.println("ALL SYSTEMS DOWN");
+    M1.setDuty(0);
+    M2.setDuty(0);
+    M3.setDuty(0);
+    M4.setDuty(0);
+    while (batteryVoltage < 11) 
+    {
+      batteryVoltage = (float)battery.getConverted();
+    }
+  }
+  else
+  {
+    //Motor test
+    for (duty=-100; duty<100; duty+=5)
+    {
+      Serial.print("Motor Duty: ");
+      Serial.println(duty);
+      M1.setDuty(duty);
+      M2.setDuty(duty);
+      M3.setDuty(duty);
+      M4.setDuty(duty);
+      delay(50);
+    }
+    for (duty=100; duty>-100; duty-=5)
+    {
+      Serial.print("Motor Duty: ");
+      Serial.println(duty);
+      M1.setDuty(duty);
+      M2.setDuty(duty);
+      M3.setDuty(duty);
+      M4.setDuty(duty);
+      delay(50);
+    }
+    
+  //Keep active the communication MKR1000 & MKRMotorCarrier
+  //Ping the samd11
+  controller.ping();
+  //wait
+  delay(50);
+  }
+}
---
+++ examples/Servo_test/Servo_test.ino
@@ -0,0 +1,85 @@
+
+#include <MKRMotorCarrier.h>
+#define INTERRUPT_PIN 6
+
+//Variable to store the battery voltage
+int batteryVoltage;
+
+void setup() 
+{
+  //Serial port initialization
+  Serial.begin(115200);
+  while (!Serial);
+
+  //Establishing the communication with the motor shield
+  if (controller.begin()) 
+    {
+      Serial.print("MKR Motor Shield connected, firmware version ");
+      Serial.println(controller.getFWVersion());
+    } 
+  else 
+    {
+      Serial.println("Couldn't connect! Is the red led blinking? You may need to update the firmware with FWUpdater sketch");
+      while (1);
+    }
+
+  // Reboot the motor controller; brings every value back to default
+  Serial.println("reboot");
+  controller.reboot();
+  delay(500);
+
+  //Take the battery status
+  float batteryVoltage = (float)battery.getConverted();
+  Serial.print("Battery voltage: ");
+  Serial.print(batteryVoltage);
+  Serial.print("V, Raw ");
+  Serial.println(battery.getRaw());
+}
+
+
+void loop() {
+
+  //Take the battery status
+  float batteryVoltage = (float)battery.getConverted();
+
+  //Reset to the default values if the battery level is lower than 11V
+  if (batteryVoltage < 11) 
+  {
+    Serial.println(" ");
+    Serial.println("WARNING: LOW BATTERY");
+    Serial.println("ALL SYSTEMS DOWN");
+    while (batteryVoltage < 11) {
+      batteryVoltage = (float)battery.getConverted();
+    }
+  }
+  else
+  {
+    //Servo sweep from 0 position to 180
+    for (int i=0; i<180; i+=5)
+    {
+      //Choose what of all the servo connectors do you want to use: servo1(default), servo2, servo3 or servo4
+      servo1.setAngle(i);
+      Serial.print("Servo position");
+      Serial.println(i);
+      delay(50);
+    }
+    
+    delay(200);
+    
+    //Servo sweep from 180 position to 0
+    for (int i=180; i>0; i-=5)
+    {
+      //Choose what of all the servo connectors do you want to use: servo1(default), servo2, servo3 or servo4
+      servo1.setAngle(i);
+      Serial.print("Servo position");
+      Serial.println(i);
+      delay(50);
+    }
+  }
+
+  //Keep active the communication MKR1000 & MKRMotorCarrier
+  //Ping the samd11
+  controller.ping();
+  //wait
+  delay(50);
+}
