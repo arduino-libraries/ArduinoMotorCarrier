222096c7ec25058c76f2c2fd4e5c504fe48d2b48
ErnestoELC <e.lopez@arduino.cc>
2021-06-15 16:31:02 +0200

    remove garbage files

--- examples/Nano/Battery_Charging/Battery_Charging.ino
+++ examples/Nano/Battery_Charging/Battery_Charging.ino
@@ -6,8 +6,6 @@ float batteryVoltage;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  //pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
   //while (!Serial);
 
@@ -32,6 +30,6 @@ void loop() {
   //Serial.println("V");
   Serial.print("V, Raw ");
   Serial.println(battery.getRaw());
-  delay(5000);                       // wait for a second
+  delay(5000); //wait for a few seconds
   
 }
